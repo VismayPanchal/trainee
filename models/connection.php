edf1efa9faf3f823ac555103e3db3f34bccdd639
VismayPanchal <vismaypb86@gmail.com>
2021-02-23 11:05:37 +0530

    fitsr commit

---
+++ models/connection.php
@@ -0,0 +1,25 @@
+<?php
+class connection
+{
+	function connect()
+	{
+
+		$host = 'localhost';
+		$user = 'root';
+		$password = 'root';
+		$db = 'Instagram';
+
+		$con = new mysqli($host,$user,$password,$db);	
+
+		if($con->connection_error)
+		{
+			echo "not connected.";
+		}
+		else
+		{
+			echo "connected";
+		}
+		return $con;
+	}
+}
+?>
